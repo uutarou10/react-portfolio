15ad5303d0ab0fa9fa307ef0f039a7d03d6c67e4
Kota Nonaka <uutarou@innovation.local>
2017-05-25 11:49:13 +0900

    Remove comment

--- src/App.js
+++ src/App.js
@@ -2,23 +2,14 @@ import React from 'react'
 
 import HeadSection from './HeadSection'
 import AboutSection from './AboutSection'
-
-//class App extends React.Component {
-//  render() {
-//    return (
-//      <div>
-//        <HeadSection title="uutarou's profile"/>
-//        <AboutSection/>
-//      </div>
-//    );
-//  }
-//}
+import WorksSection from './WorksSection'
 
 const App = () => {
   return (
     <div>
       <HeadSection title="uutarou's profile"/>
       <AboutSection/>
+      <WorksSection />
     </div>
   );
 }
