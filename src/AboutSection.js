bef56a79731b15f41f003769165189fbdb5058ab
Kota Nonaka <uutarou@innovation.local>
2017-05-26 10:42:15 +0900

    fix typo

--- src/AboutSection.js
+++ src/AboutSection.js
@@ -5,7 +5,7 @@ import Table from './Table'
 const list = [
   {th:'Name',td:'Kota Nonaka'},
   {th:'School',td:'Tokyo University of Technology'},
-  {th:'Loacation',td:'Kanagawa, Japan'},
+  {th:'Location',td:'Kanagawa, Japan'},
   {th:'Twitter',td:<a href="http://twitter.com/uutarou10" target="__blank">@uutarou10</a>},
   {th:'Blog',td:<a href="http://yurufuwa-tech.hatenablog.com" target="__blank">ゆるふわ技術日誌</a>},
   {th:'Github',td:<a href="https://github.com/uutarou10" target="__blank">uutarou10</a>},
